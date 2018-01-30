251ec773008971e979ff90fc83f2718e8013ffdc
sanattaori <sanattaori3@gmail.com>
2018-01-30 23:51:42 +0530

    player data

--- player/views.py
+++ player/views.py
@@ -5,4 +5,15 @@ from gameplay.models import Game
 
 
 def home(request):
-    return render(request, "player/home.html", {'ngames': Game.objects.count()})
+    games_first_player = Game.object.filter(
+        first_player = request.user,
+        status = 'F'
+    )
+    games_second_player = Game.object.filter(
+        second_player = request.user,
+        status = 'S'
+    )
+    all_games = list(games_first_player) + list(games_second_player)
+
+    return render(request, "player/home.html",
+                  {'games': all_games})
